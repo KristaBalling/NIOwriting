0bd3155bf26abfd6487ee13141f65dab927c52e5
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-11 15:33:59 -0500

    FileChannel to copy files

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -5,28 +5,51 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.Pipe;
 
 //Java NIO Mantra:
 //when something goes wrong: .flip()
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            Pipe pipe = Pipe.open();
+
+            Runnable writer = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Pipe.SinkChannel sinkChannel = pipe.sink();
+                        ByteBuffer buffer = ByteBuffer.allocate(56);
+
+                        for(int i=0; i<10; i++) {
+                            String currentTime = "The time is: " + System.currentTimeMillis();
+
+                            buffer.put(currentTime.getBytes());
+                            buffer.flip();
+
+                            while(buffer.hasRemaining()) {
+                                sinkChannel.write(buffer);
+                            }
+                            buffer.flip();
+                            Thread.sleep(100);
+
+                        }
+
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
         try (FileOutputStream binFile = new FileOutputStream("data.dat");
             FileChannel binChannel = binFile.getChannel()) {
 
 
             ByteBuffer buffer = ByteBuffer.allocate(100);
-
-//            byte[] outputBytes = "Hello World!".getBytes();
-//            byte[] outputBytes2 = "Nice to meet you".getBytes();
-//            buffer.put(outputBytes).putInt(245).putInt(-98765).put(outputBytes2).putInt(1000);
-//            buffer.flip();
-
-//            buffer.putInt(245);
-//            buffer.putInt(-98765);
-//            buffer.put(outputBytes2);
-//            buffer.putInt(1000);
-//            buffer.flip();
 
             byte[] outputBytes = "Hello World!".getBytes();
             buffer.put(outputBytes);
@@ -64,7 +87,18 @@ public class Main {
 
             System.out.println("int1 =" + readBuffer.getInt());
 
-            byte[] outputString = "Hello, World!".getBytes();
+            RandomAccessFile copyFile = new RandomAccessFile("datacopy.dat", "rw");
+            FileChannel copyChannel = copyFile.getChannel();
+//            long numTransferred = copyChannel.transferFrom(channel, 0, channel.size());
+            long numTransferred = channel.transferTo(0, channel.size(), copyChannel);
+            System.out.println("Num transferred = " + numTransferred);
+
+            channel.close();
+            ra.close();
+            copyChannel.close();
+
+
+            byte[] outputString = "Hello World!".getBytes();
             long str1Pos = 0;
             long newInt1Pos = outputString.length;
             long newInt2Pos = newInt1Pos + Integer.BYTES;
@@ -79,27 +113,21 @@ public class Main {
             binChannel.write(intBuffer);
 
             intBuffer.flip();
-            intBuffer.putInt(-987654);
-            intBuffer.flip();
-            binChannel.position(newInt2Pos);
-            binChannel.write(intBuffer);
-
-            intBuffer.flip();
-            intBuffer.putInt(1000);
-            intBuffer.flip();
-            binChannel.position(newInt3Pos);
-            binChannel.write(intBuffer);
-
-            binChannel.position(str1Pos);
-            binChannel.write(ByteBuffer.wrap(outputString));
-            binChannel.position(str2Pos);
-            binChannel.write(ByteBuffer.wrap(outputString2));
 
 
-
+//            RandomAccessFile copyFile = new RandomAccessFile("datacopy.dat", "rw");
+//            FileChannel copyChannel = copyFile.getChannel();
+//            channel.position(0);
+////            long numTransferred = copyChannel.transferFrom(channel, 0, channel.size());
+//            long numTransferred = channel.transferTo(0, channel.size(), copyChannel);
+//            System.out.println("Num transferred = " + numTransferred);
+//
+//            channel.close();
+//            ra.close();
+//            copyChannel.close();
 
         } catch(IOException e) {
             e.printStackTrace();
         }
-    }
+   }
 }
